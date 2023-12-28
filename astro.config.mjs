8a6fe9dfc5f6fc53d2102213e9076bfa4a8d514e
Geronimo Gonzalez C <geronimo24gonzalez@gmail.com>
2023-12-28 13:45:03 -0500

    deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	base :"/mi-repo",
+	site: 'https://geronimogc.github.io/',
+	base :"/ATRO-PRUEBA",
 	integrations: [mdx(), sitemap()],
 });
